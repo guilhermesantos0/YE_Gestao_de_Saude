e4ee50dd07239461d296d84ebdafcb27ed37eb24
guilhermesantos0 <guilhermesantosdev@outlook.com>
2024-05-27 15:21:16 -0300

    a

--- back_end/index.js
+++ back_end/index.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 // const config = require('../config.json');
 
 console.log(process.env)
 
 const app = express();
-const port = 3000;
-
 app.use(bodyParser.json());
 
-// const db = mysql.createConnection({
-//     host: config.mysql.host,
-//     user: 'root',
-//     password: 'mysqlguilherme',
-//     database: 'mydatabase'
-// })
+const db = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDB
+})
+
+app.post('/addConsulta', (req, res) => {
+    console.log(req.body)
+    res.status(200)
+})
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Servidor rodando na porta 3000')
 })
