d585274bc21210214e873bb40323babfcde8d057
Murilo Orsi Marchezzane <133171713+muriloorsi@users.noreply.github.com>
2024-05-29 19:05:32 -0300

    correç

--- front_end/App.js
+++ front_end/App.js
@@ -4,8 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator()
-
-
+import Login from './src/pages/Login';
 import Home from './src/pages/Home';
 import EditProfile from './src/pages/Perfil';
 import Cadastro from './src/pages/Cadastro/Main';
@@ -23,7 +22,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-
+        <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='Home' component={Home} options={{ title: 'YE Gestão de Saúde', headerShown: false }}  />
         <Stack.Screen name='Perfil' component={EditProfile} />
         <Stack.Screen name='Cadastro' component={Cadastro}></Stack.Screen>
