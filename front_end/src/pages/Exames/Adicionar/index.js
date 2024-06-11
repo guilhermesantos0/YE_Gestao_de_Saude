a8b5c45b47f2ded49a714a600be493ed8fc5896b
muriloorsi <muriloo.marchezzane@gmail.com>
2024-06-11 19:32:42 -0300

    fVSDB

--- front_end/src/pages/Exames/Adicionar/index.js
+++ front_end/src/pages/Exames/Adicionar/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './style'; 
@@ -30,32 +30,4 @@ const Exames = ({ navigation }) => {
 };
 
 export default Exames;
-=======
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import styles from "./style";
 
-const Adicionar = () => {
-    const handleButton1Press = () => {
-        console.log('Botão 1 pressionado');
-    };
-
-    const handleButton2Press = () => {
-        console.log('Botão 2 pressionado');
-    };
-
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Escolha a forma de logar</Text>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Manual')}>
-                <Text style={styles.buttonText}>Adicionar com leitura de PDF</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={handleButton2Press}>
-                <Text style={styles.buttonText}>Adicionar Manualmente</Text>
-            </TouchableOpacity>
-        </View>
-    );
-};
-
-export default Adicionar;
->>>>>>> eb7df2d64868778d2ce61725ba53756030cfc817
